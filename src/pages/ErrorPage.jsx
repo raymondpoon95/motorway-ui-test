bb9fb45f8665d4920294236cc35be11312c93e56
Raymond Poon <raymondpoon95@hotmail.co.uk>
2023-03-31 18:16:32 +0100

    error page

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <h1>404 Error. Page not found.</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
